b784c88e223f980adad19963c9aade3c61ad4562
riipah <riipah@gmail.com>
2014-08-09 15:30:24 +0300

    docs

--- BulkEnvelopeEditor/Note.cs
+++ BulkEnvelopeEditor/Note.cs
@@ -6,8 +6,14 @@ namespace BulkEnvelopeEditor {
 
 		public string Envelope { get; set; }
 
+		/// <summary>
+		/// Original line number of the Envelope parameter.
+		/// </summary>
 		public int EnvelopeLineNumber { get; set; }
 
+		/// <summary>
+		/// Whether the lyric of this note represents a breath or rest sound.
+		/// </summary>
 		public bool IsBreathOrRest {
 			get {
 				
@@ -18,12 +24,21 @@ namespace BulkEnvelopeEditor {
 			}
 		}
 
+		/// <summary>
+		/// Whether the note was selected in UTAU for editing.
+		/// 
+		/// UTAU automatically includes previous and next notes from the selections.
+		/// For those notes, this will be false.
+		/// </summary>
 		public bool IsSelectedInUtau { get; set; }
 
 		public int Length { get; set; }
 
 		public string Lyric { get; set; }
 
+		/// <summary>
+		/// Line number where this note ends.
+		/// </summary>
 		public int NoteEndLine { get; set; }
 
 	}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-utauBulkEnvelopeEditor
+UTAU Bulk Envelope Editor
 ======================
 
-Plugin for UTAU, for bulk-editing note envelopes in UST files
+Plugin for UTAU, for bulk-editing note envelopes in UST files.
+
+## How to use ##
+
+1. Select one or more notes in UTAU.
+2. Start the plugin (from Tools -> Plug-ins -> BulkEnvelopeEdit)
+3. Enter p and v values that you wish to be changed. Values not entered are not touched. You may also enter a minimum length for the notes where the editor will apply.
+4. Press the button which applies the changes and closes the plugin.
+
+## Installation ##
+
+1. Build or download the binary.
+2. Copy the binary AND plugin.txt to the plugin folder under UTAU's installation folder, for example C:\UTAU\plugins\bulkEnvelopeEdit
